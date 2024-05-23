a666e1da6cb14bcbbe23d95ee180e045a01fe1e8
John Ryan Andal <jrc.andal28@gmail.com>
2024-05-23 10:47:55 +0800

    Posts Search Via Content or Title

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2c8914813153928b4da70461b6056b99ce7c41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578125963d4559ca98e5f5ec89c62067f0a3d499")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
